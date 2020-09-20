8239744e449268db9a0e7c57bba64688b7e08fac
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-09-19 20:08:18 -0400

    removed links because this is a single page app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Search from "./pages/Search";
-import Favorites from './pages/Favorites';
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <Navbar />
-        <Wrapper>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/search" component={Search} />
-          <Route exact path="/favorites" component={Favorites} />
-        </Wrapper>
-        <Footer />
-      </div>
-    </Router>
-  );
+  return <Home/>
 }
 
 export default App;
