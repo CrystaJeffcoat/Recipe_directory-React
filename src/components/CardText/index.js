f5bd72648f0597e7e30242abe1ccea5069515996
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-09-19 20:15:55 -0400

    removed extra text that wasn't needed

--- src/components/CardText/index.js
+++ src/components/CardText/index.js
@@ -5,7 +5,7 @@ function CardText(props) {
     <ul className='list-group' key={props.id}>
       {props.oils.map((item, index) => (
         <li className='list-group-item' key={props.id + '-' + index.toString()}>
-          {item.amount} {item.name} {props.id + '-' + index.toString()}
+          {item.amount} {item.name}
         </li>
       ))}
     </ul>
