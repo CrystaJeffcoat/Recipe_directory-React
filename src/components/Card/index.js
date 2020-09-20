5cfa8595ae36160850feb453815bf0550650c636
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-09-19 20:12:39 -0400

    replaced components with props in order to change state of cardbtn

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,23 +1,13 @@
 import React from "react";
-import CardBtn from "../CardBtn";
-import CardText from '../CardText';
 import "./style.css";
 
 function Card(props) {
+  
   return (
     <div className="card" {...props}>
-      {!props.image && <i className="fa fa-spinner fa-spin" aria-hidden="true" />}
       <div className='card-body'>
         <h5 className='card-title'>{props.title}</h5>
-        <CardText 
-          oils = {props.ingredients}
-          id = {props.id}
-        />
-        <CardBtn
-          style={{ opacity: props.image ? 1 : 0 }}
-          onClick={props.handleBtnClick}
-          data-value="pass"
-        />
+          <div {...props}></div>
       </div>
     </div>
   );
